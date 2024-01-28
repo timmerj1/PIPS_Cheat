bbaf8910f08499fb7afb0e2a87404e668c5e992c
Jeroen Timmerman <timmerman.jeroen@gmail.com>
2024-01-28 22:06:36 +0100

    Wheel turn + setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # Needed to actually package something
     packages=['pipscheat'],
     # Needed for dependencies
-    install_requires=['numpy'],
+    install_requires=['numpy', 'importlib_resources'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
