587b2217a7c00f0469aa5658ae06399b89a4a88c
Jeroen Timmerman <timmerman.jeroen@gmail.com>
2024-01-28 22:10:38 +0100

    Setup changes

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # Needed for dependencies
     install_requires=['numpy', 'importlib_resources'],
     # *strongly* suggested for sharing
-    version='0.1',
+    version='0.1.1',
     # The license can be anything you like
     license='MIT',
     description='Python packeage to cheat on PIPS Python assignments.',
